a49af1542f1d3da67e7a77cc3866f8a1b21a7d80
Zilu Zhou <zz1994@citadel.cosmo.fas.nyu.edu>
2021-07-22 17:02:50 -0400

    Fourier implementation

--- CIBxLIM.py
+++ CIBxLIM.py
@@ -9,6 +9,8 @@ import vegas
 from scipy import interpolate
 from scipy import integrate
 import matplotlib.pyplot as plt
+import time
+
 
 """
 Unit System:
@@ -48,6 +50,7 @@ class CIBxLIM():
         else:
             print("Sorry, only EXCLAIM experiment is supported at the moment.")
 
+        self.L = np.array(cosmo.comoving_distance(self.z_X_max)-cosmo.comoving_distance(self.z_X_min))
         self.l_X = 158 # Rest wavelength of CII, in microns.
         self.ell = 0
         self.kp = 0
@@ -72,9 +75,9 @@ class CIBxLIM():
         """
         # Tuple random points P(z, k) interpolator.
         print("Loading and creating P(z, k) interpolator...")
-        PK_grid = np.loadtxt("PKdata/k0-40_3500.txt")
-        k = np.linspace(0, 1, 3500)
-        z = np.linspace(self.z_min, self.z_max, 3500)
+        PK_grid = np.loadtxt("PKdata/k0-1_1000.txt")
+        k = np.linspace(0, 1, 1000)
+        z = np.linspace(self.z_min, self.z_max, 1000)
         self.PK_interpolator = interpolate.RectBivariateSpline(z, k, PK_grid)
         print("P(z, k) interpolator successfully created.")
 
@@ -211,28 +214,84 @@ class CIBxLIM():
         CIB = self.CIBmodel.CIB_model(l, z) # (dim(l), dim(z))
         return scaling * bias * pkz * CIB
 
-    def F_hat(self, kpp, z_arr):
+    def F_hat(self, z_arr, idx=500):
         chi_X_min = np.array(cosmo.comoving_distance(self.z_X_min))
         chi_X_max = np.array(cosmo.comoving_distance(self.z_X_max))
-        chi_X_arr = np.linspace(chi_X_min, chi_X_max, 1000) # Evenly spaced comoving distance points.
+        #M = int(np.ceil(5*(chi_X_max - chi_X_min)*idx/100))
+        M=20000
+        #print(M)
+        #chi_X_min = np.array(cosmo.comoving_distance(0.03))
+        #chi_X_max = np.array(cosmo.comoving_distance(1))
+        chi_X_arr = np.linspace(chi_X_min, chi_X_max, M) # Evenly spaced comoving distance points.
+         
         z_X_arr = self.z_from_chi(chi_X_arr) # Convert to redshift, not evenly spaced. 
         l_arr = (1+z_X_arr) * self.l_X # In micrometers
-        
+     
         CIB = self.CIBmodel.CIB_model(l_arr, z_arr) # CIB model data grid. Shape is (dim(l), dim(z))
         chip = self.chi_p(l_arr)
         func = CIB / chip.reshape((chip.size, 1)) # CIB model / peak comoving distance.
         
-        func_k = np.fft.fft(func, axis=0) # Apply Fourier transform to l axis.
+        func_k = np.fft.fft(func, axis=0, norm=None) # Apply Fourier transform to l axis. 
         k = np.fft.fftfreq(chi_X_arr.size, d=chi_X_arr[1] - chi_X_arr[0])
+         
         func_k = func_k[np.where(k>=0), :][0] # We only want part of function at positive k. 
         k = k[np.where(k>=0)] # Filter out negative k.
+        
+        # Data saving
+        #np.savetxt("FHATdata/FHAT.txt", func_k*(chi_X_arr[1]-chi_X_arr[0]), fmt=["%.4e%+.4ej"]*1000)
+        #np.savetxt("FHATdata/k.txt", k)
 
-        closest_idx = np.argmin(np.abs(k - kpp)) # Index of the k value closest to input kpp.
-        return func_k[closest_idx, :] # Return row of transformed function corresponding to kpp. Shape is (dim(z),)
+        #return k[idx], func_k[idx, :]*(chi_X_arr[1] - chi_X_arr[0]) # Return row of transformed function corresponding to kpp. Shape is (dim(z),)
+        return k, func_k*(chi_X_arr[1] - chi_X_arr[0])       
 
-    def V_tilde(self, kp, kpp, ell, neval=10000):
-        z_arr = np.linspace(self.z_min, self.z_max, neval)
-        F_hat_func = self.F_hat(kpp, z_arr)
+    def F_hat_int1(self, kpp, z_arr):
+        l_min = (1+self.z_X_min) * self.l_X 
+        l_max = (1+self.z_X_max) * self.l_X
+        #l_min = 1.03 * self.l_X
+        #l_max = 2 * self.l_X
+        l_arr = np.linspace(l_min, l_max, 100000)
+        """
+        CIB = self.CIBmodel.CIB_model(l_arr, z_arr) # CIB model data grid. Shape is (dim(l), dim(z))
+        #chip = self.chi_p(l_arr)
+        scale = (const.c/1000) / self.l_X / np.array(cosmo.H(self.z_X(l_arr)))
+        func = scale.reshape((scale.size, 1)) * CIB #/ chip.reshape((chip.size, 1)) # CIB model / peak comoving distance.
+        """
+        func = self.chi_p(l_arr)
+        xpp = np.array(cosmo.comoving_distance(self.z_X(l_arr)))
+
+        expo = np.exp(-1j*kpp*xpp*2*np.pi)
+
+        func = func * expo.reshape((expo.size, 1))
+        return integrate.simps(y=func, x=l_arr, axis=0) 
+
+    def F_hat_int2(self, kpp, z_arr):
+        chi_X_min = np.array(cosmo.comoving_distance(self.z_X_min))
+        chi_X_max = np.array(cosmo.comoving_distance(self.z_X_max))
+        
+        #chi_X_min = np.array(cosmo.comoving_distance(0.03))
+        #chi_X_max = np.array(cosmo.comoving_distance(1))
+        
+        chi_X_arr = np.linspace(chi_X_min, chi_X_max, 5000) # Evenly spaced comoving distance points.
+        z_X_arr = self.z_from_chi(chi_X_arr) # Convert to redshift, not evenly spaced. 
+        l_arr = (1+z_X_arr) * self.l_X # In micrometers
+
+        CIB = self.CIBmodel.CIB_model(l_arr, z_arr) # CIB model data grid. Shape is (dim(l), dim(z))
+        chip = self.chi_p(l_arr)
+        func = CIB / chip.reshape((chip.size, 1)) # CIB model / peak comoving distance.
+       
+        #func = 1/self.chi_p(l_arr)
+
+        expo = np.exp(1j*kpp*chi_X_arr*2*np.pi)
+
+        func = func * expo.reshape((expo.size, 1))
+
+        return integrate.simps(y=func, x=chi_X_arr, axis=0)  
+
+
+    def V_tilde(self, kp, ell, neval=1000):
+        start = time.perf_counter()
+        z_arr = np.linspace(self.z_min, 5, neval)
+
         bias = self.b_eff(z_arr)
         xp = np.array(cosmo.comoving_distance(z_arr)) # x parallel, comoving distance at CIB redshifts.
         k_arr = np.sqrt(kp**2 + ell**2 / xp**2) # Spherical k  mode.
@@ -240,17 +299,54 @@ class CIBxLIM():
 
         sinu_arg = -1j * kp * xp
 
-        func_total = np.exp(sinu_arg) * bias * pkz * F_hat_func
-
-        return integrate.simps(y=func_total, x=z_arr)
-    
-    def fourier_Cl(self, kpp, ell):
-        kp_arr = np.linspace(0, 0.5, 150) # k parallel to integrate over.
-        V_arr = np.zeros(kp_arr.size, dtype="complex128")
-        for i in range(kp_arr.size):
-            V_arr[i] = self.V_tilde(kp_arr[i], kpp, ell)
-        V_arr_conj = V_arr.conj() # Take conjugate.
-        res = integrate.simps(y=V_arr * V_arr_conj, x=kp_arr) # Integrate V*V, along k parallel. 
+        func_total = np.exp(sinu_arg) * bias * pkz * self.fhat
+        res = integrate.simps(y=func_total, x=z_arr, axis=-1)
+        #print(time.perf_counter() - start)
         return res
+    
+    """
+    Computes C_l for input ell at several values of kpp.
+    This way more than one entry of the FFT may be used per calculation.
+
+    Params:
+    ------
+    (1d array) ell_arr : Array containing the ell values to be sampled at. This is the time
+    consuming part as the nature of the matter power spectrum does not allow entire ell arrays
+    to be sampled in parallel.
+
+    Return:
+    ------
+    (1d array) kpp_arr : Array containing the k_{\parallel}' values corresponding to each row in the
+    C_l matrix.
+    (2d array) cls : Matrix containing the k_{\parallel}' and the ell entries of the CIB C_l. the
+    rows correspond to a kpp value, and columns are ells identical to that of input ell_arr.
+    """
+    def fourier_Cl(self, ell_arr):
+        kp_arr = np.linspace(0, 0.5, 50) # k_{\parallel} array to integrate over. 
+        z_arr = np.linspace(self.z_min, 5, 1000) # Redshift is cut off at z=5 as the CIB diminishes.
+        kpp_arr, self.fhat = self.F_hat(z_arr) # Line associated Fourier componenet. 
+        
+        idx = np.linspace(3, 200, 10, dtype="int") # kpp values are idx/1000
+        kpp_arr = kpp_arr[idx]
+        self.fhat = self.fhat[idx]
+        
+        # Beginning of main computation
+        start = time.perf_counter()
+        res = np.full(kpp_arr.size, -1, dtype="float64") # Holder result array filled with -1. 
+        # Loop through ells:
+        for i, ell in enumerate(ell_arr):
+            print(i)
+            V_arr = np.zeros((kp_arr.size, kpp_arr.size), dtype="complex128")
+            for i in range(kp_arr.size): 
+                V_arr[i] = self.V_tilde(kp_arr[i], ell) # Compute V_tilde at all values of k_{\parallel}
+            V_arr_conj = V_arr.conj() # Take conjugate.
+            subres = integrate.simps(y=V_arr * V_arr_conj, x=kp_arr, axis=0) # Integrate V*V, along k_{parallel} 
+            if np.all(res == -1):
+                res = subres.real
+            else:
+                res = np.column_stack((res, subres.real))
+        print(time.perf_counter()-start)
+        cls = res/2/np.pi/self.L
+        return kpp_arr, cls
 
 
--- fourier_test.py
+++ fourier_test.py
@@ -4,32 +4,75 @@ from CIBxLIM import CIBxLIM
 from astropy.cosmology import Planck18_arXiv_v2 as cosmo
 from scipy.interpolate import interp1d
 import time
+from numba import njit
 
 CIB = CIBxLIM()
 larr = np.linspace(158*3.5, 158*4.5, 1000)
-zarr = np.linspace(CIB.z_min, CIB.z_max, 8000)
+zarr = np.linspace(CIB.z_min, 5, 1000)
 #kp = np.linspace(0, 1, 10)
-kp = 0.2
+kpp = 0.003
 ell = 10
 
 chi = lambda z : np.array(cosmo.comoving_distance(z))
 l_from_z = lambda z : 158*(1+z) # Convert z to line frequency.
 z_from_chi = interp1d(chi(zarr), zarr) # Interpolate z off of comoving distance.
 
+#print(CIB.V_tilde(kpp, kpp, ell))
+
+cls50 = np.loadtxt("cls50.txt")
+cls100 = np.loadtxt("cls.txt")
+cls200 = np.loadtxt("cls200.txt")
+
+ell=np.arange(200)
+plt.loglog(ell, cls50[0], label="N=50")
+plt.loglog(ell, cls100[0, :200], label="N=100")
+plt.loglog(ell, cls200[0], label="N=200")
+plt.legend()
+plt.show()
+
+
 """
-#print(CIB.CIBmodel.CIB_model(larr, zarr).shape)
-kpp_arr = np.linspace(0.02, 0.48, 6)
-for kpp in kpp_arr:
-    plt.plot(zarr, CIB.F_hat(kpp, zarr), label="{:.3f}".format(kpp))
+# Making Cls
+ell = np.arange(200)
+kpp, cls = CIB.fourier_Cl(ell)
+np.savetxt("cls50.txt", cls)
+#np.savetxt("kpp.txt", kpp)
+
+
+for i, cl in enumerate(cls):
+    plt.loglog(ell, cl, label="{:.4f}".format(kpp[i]))
+plt.legend()
+plt.xlabel(r"$\ell$")
+plt.ylabel(r"$C_{\ell}$")
+plt.show()
+"""
+
+"""
+# F_hat test
+start = time.perf_counter()
+#kval, f1 = CIB.F_hat(kpp, zarr)
+#karr = np.loadtxt("FHATdata/k.txt")
+#func_k = np.loadtxt("FHATdata/FHAT.txt", dtype=np.complex_)
+#kval = karr[500]
+#f1 = func_k[500]
+print(time.perf_counter() - start)
+
+start = time.perf_counter()
+f2 = CIB.F_hat_int2(kval, zarr)
+print(time.perf_counter() - start)
+
+print("k value is {}".format(kval))
+
+plt.plot(zarr, abs(f1), label="FFT")
+plt.plot(zarr, abs(f2), "--", label="Simpsons")
+
 plt.xlabel("z")
-plt.ylabel(r"$\hat{F}(k_{\parallel}', z)$")
+plt.ylabel(r"$\hat{F}$")
+plt.title(r"$k_{\parallel}' = $" + "{:.4f} 1/Mpc".format(kval))
 plt.legend()
 plt.show()
 """
 
-start = time.perf_counter()
-print(CIB.fourier_Cl(0.1, 20))
-print(time.perf_counter() - start)
 
 
 
@@ -37,42 +80,3 @@ print(time.perf_counter() - start)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-xparr1 = np.linspace(1000, 2000, 1000)
-xparr2 = np.linspace(6000, 7000, 1000)
-
-kparr1 = np.fft.fftfreq(xparr1.size, d=xparr1[1] - xparr1[0])
-kparr2 = np.fft.fftfreq(xparr2.size, d=xparr2[1] - xparr2[0])
-#print(kparr1[np.where(kparr1>=0)])
-#print(kparr2[np.where(kparr2>=0)])
-
-ll1, zz1 = np.meshgrid(larr, z_from_chi(xparr1))
-ll2, zz2 = np.meshgrid(larr, z_from_chi(xparr2))
-
-func1 = CIB.CIBmodel.CIB_model(ll1, zz1)
-func2 = CIB.CIBmodel.CIB_model(ll2, zz2)
-
-funck1 = np.fft.fft(func1, axis=-1)
-funck2 = np.fft.fft(func2, axis=-1)
-
-plt.plot(kparr1, funck1[:, 3])
-plt.plot(kparr2, funck2[:, 3])
-plt.show()
-"""
